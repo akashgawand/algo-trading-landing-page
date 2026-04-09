5eec075bb9c00a9cfa84b9e0159da5bd90e5d054
akashgawand <akashgawand2004@gmail.com>
2026-04-10 01:51:02 +0530

    feat: initial commit

---
+++ app/pages/landing/page.jsx
@@ -0,0 +1,10 @@
+import Globe3D from "@/components/ui/hero";
+
+export default function LandingPage() {
+  return (
+    <main className="min-h-screen bg-[#0a0613]">
+      <Globe3D />
+      {/* Additional sections can be added here */}
+    </main>
+  );
+}
